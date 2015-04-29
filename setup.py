36cf6579117f785e1943ac6bd6b923a2a6a8a2bf
Jeff Dairiki <dairiki@dairiki.org>
2015-04-29 09:48:13 -0700

    Add LICENSE and README

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ from setuptools.command.test import test as TestCommand
 
 VERSION = '0.1.dev0'
 
-#here = os.path.abspath(os.path.dirname(__file__))
-#README = open(os.path.join(here, 'README.rst')).read()
-#CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
 install_requires = ['six']
 
@@ -17,6 +17,7 @@ tests_require = ['pytest']
 
 if sys.version_info < (2, 7):
     install_requires.append('ordereddict')
+
 
 class PyTest(TestCommand):
     user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
@@ -31,7 +32,7 @@ class PyTest(TestCommand):
         self.test_suite = True
 
     def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
+        # import here, cause outside the eggs aren't loaded
         import pytest
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
@@ -39,14 +40,23 @@ class PyTest(TestCommand):
 setup(name='libtcd',
       version=VERSION,
       description='Interface to xtide’s libtcd',
-      #long_description=README + '\n\n' + CHANGES,
+      long_description=README + '\n\n' + CHANGES,
       classifiers=[
-        "Programming Language :: Python",
-        # FIXME
-        ],
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: BSD License",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: Implementation :: CPython",
+          "Topic :: Scientific/Engineering",
+          "Topic :: Software Development :: Libraries",
+          ],
       author='Jeff Dairiki',
       author_email='dairiki@dairiki.org',
-      url='',
+      url='https://github.com/dairiki/python-libtcd',
       keywords='xtide',
 
       cmdclass={'test': PyTest},
