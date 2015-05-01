83826811e9a80ff683f2edcd3820c93687efb269
Jeff Dairiki <dairiki@dairiki.org>
2015-05-01 07:25:10 -0700

    Testing, flake8

--- libtcd/compat.py
+++ libtcd/compat.py
@@ -6,14 +6,14 @@ from __future__ import absolute_import
 from six import text_type
 
 try:
-    from collections import OrderedDict ; 'SIDE-EFFECTS'
-except ImportError:                     # pragma: NO COVER
-    from ordereddict import OrderedDict ; 'SIDE-EFFECTS'
+    from collections import OrderedDict  # noqa
+except ImportError:                      # pragma: NO COVER
+    from ordereddict import OrderedDict  # noqa
 
 
 def bytes_(s, encoding='latin-1', errors='strict'):
     """ If ``s`` is an instance of ``text_type``, return
     ``s.encode(encoding, errors)``, otherwise return ``s``"""
-    if isinstance(s, text_type):  # pragma: no cover
+    if isinstance(s, text_type):
         return s.encode(encoding, errors)
     return s
