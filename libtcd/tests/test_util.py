49bc113a96e166fcc15ef5ceea4773d39953f6a1
Jeff Dairiki <dairiki@dairiki.org>
2015-05-01 07:29:13 -0700

    Add .util.reify
    (Ripped off from pyramid)

--- libtcd/tests/test_util.py
+++ libtcd/tests/test_util.py
@@ -4,6 +4,8 @@
 from __future__ import absolute_import
 
 from datetime import timedelta
+import unittest
+
 import pytest
 
 
@@ -59,3 +61,34 @@ class Test_remove_if_exists(object):
         testdir = tmpdir.mkdir('testdir')
         with pytest.raises(OSError):
             self.call_it(testdir.strpath)
+
+
+class TestReify(unittest.TestCase):
+    # Ripped verbatim from pyramid.tests.test_decorator
+    def _makeOne(self, wrapped):
+        from ..util import reify
+        return reify(wrapped)
+
+    def test___get__withinst(self):
+        def wrapped(inst):
+            return 'a'
+        decorator = self._makeOne(wrapped)
+        inst = Dummy()
+        result = decorator.__get__(inst)
+        self.assertEqual(result, 'a')
+        self.assertEqual(inst.__dict__['wrapped'], 'a')
+
+    def test___get__noinst(self):
+        decorator = self._makeOne(None)
+        result = decorator.__get__(None)
+        self.assertEqual(result, decorator)
+
+    def test___doc__copied(self):
+        def wrapped(inst):
+            """My doc"""
+        decorator = self._makeOne(wrapped)
+        self.assertEqual(decorator.__doc__, "My doc")
+
+
+class Dummy(object):
+    pass
--- libtcd/util.py
+++ libtcd/util.py
@@ -36,3 +36,28 @@ def remove_if_exists(filename):
     except OSError as ex:
         if ex.errno != errno.ENOENT:
             raise
+
+
+class reify(object):
+    """ Use as a class method decorator.  It operates almost exactly like the
+    Python ``@property`` decorator, but it puts the result of the method it
+    decorates into the instance dict after the first call, effectively
+    replacing the function it decorates with an instance variable.  It is, in
+    Python parlance, a non-data descriptor.  An example:
+
+    Ripped verbatim from :class:`pyramid.decorator.reify`.
+
+    """
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+        try:
+            self.__doc__ = wrapped.__doc__
+        except:  # pragma: no cover
+            pass
+
+    def __get__(self, inst, objtype=None):
+        if inst is None:
+            return self
+        val = self.wrapped(inst)
+        setattr(inst, self.wrapped.__name__, val)
+        return val
